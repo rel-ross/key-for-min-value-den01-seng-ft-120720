2ce24f248ec299252fc49dc4003eb7043f4e4f6f
Arielle ross <arielle@arielleross.com>
2020-11-26 01:29:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,7 @@
 
 def key_for_min_value(name_hash)
 smallest_value = name_hash.first[1]
+smallest_key = []
   name_hash.each do |key, value|
     if value < smallest_value 
       smallest_value = value
@@ -14,6 +15,6 @@ smallest_value = name_hash.first[1]
       smallest_key = key
   end
   end
-  return key
+  return smallest_key
 end
 
