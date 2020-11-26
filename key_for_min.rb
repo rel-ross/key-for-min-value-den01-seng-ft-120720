feca157b522868ffe60b0774b6caa09270ee56a1
Arielle ross <arielle@arielleross.com>
2020-11-26 01:16:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,7 @@
 
 def key_for_min_value(name_hash)
 smallest_value = name_hash.first[1]
+smallest_key
   name_hash.each do |key, value|
     if value < smallest_value 
       smallest_value = value
@@ -11,6 +12,7 @@ smallest_value = name_hash.first[1]
   end
   name_hash.each do |key, value|
     if value == smallest_value
+      smallest_key = key
       return key
   end
 
